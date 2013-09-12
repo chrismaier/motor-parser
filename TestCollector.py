a2ba6620ae3a371b4aa4be2955c92ae48ecea7e6
Greg Swift <gregswift@gmail.com>
2013-09-12 10:27:15 -0500

    should have been referencing the returned object, not the data returned by the get function

--- TestCollector.py
+++ TestCollector.py
@@ -3,6 +3,6 @@ import MotorParser
 
 p = MotorParser.MotorParser()
 temp = p.get_motor_info("http://pro38.com/products/pro75/motor/MotorData.php?prodid=5069L1685-P")
-temp.print_output_header()
-temp.print_motor_data(temp)
+p.print_output_header()
+p.print_motor_data(temp)
 p.close()
